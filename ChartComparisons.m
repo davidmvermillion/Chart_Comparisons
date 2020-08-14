7ac1b991a4434923f699510a5a1424b352113665
David M Vermillion <social@davidmvermillion.com>
2020-08-14 19:20:09 -0400

    Matlab start
    Scatterplot started

---
+++ ChartComparisons.m
@@ -0,0 +1,5 @@
+% scatterplot
+scatter(Labels, Series_1), xlabel('Datapoint'), ylabel('Datapoint Value'), title('Randomized Data Points')
+hold on
+scatter(Labels, Series_2), xlabel('Datapoint'), ylabel('Datapoint Value'), title('Randomized Data Points')
+hold off
