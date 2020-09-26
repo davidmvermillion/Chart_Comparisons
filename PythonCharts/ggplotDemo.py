b15de6af5c9f4a0e848206b43edd233f62eb2bac
David M Vermillion <social@davidmvermillion.com>
2020-09-26 14:53:41 -0400

    Python ggplot Project Created
    ggplotDemo.py created and outlined

---
+++ PythonCharts/ggplotDemo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 14:51:13 2020
+
+@author: david
+"""
+# Import packages
+import pandas as pd
+import numpy as np
+import ggplot as gg
+
+# Import data
+
+# Structure data
+
+# plot scatterplot in ggplot
