6696041fa87ae8959e1453ce7814b0e4ed566210
David M Vermillion <social@davidmvermillion.com>
2020-09-27 18:40:18 -0400

    Partial work on ggplot2 scatterplot

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -1,1 +1,10 @@
+# Load the tidyverse, which includes ggplot2
+
 library('tidyverse')
+
+# Import CSV as tibble
+a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_for_Comparison_Project.csv", col_names = FALSE)
+
+# Scatterplot
+ggplot(data = a) +
+  geom_point(mapping = aes(x = x1, y = x2))
