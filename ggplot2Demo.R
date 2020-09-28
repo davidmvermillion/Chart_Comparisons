3922c65b1221292d3f7b637d8b8d0e1a55451fc0
David M Vermillion <social@davidmvermillion.com>
2020-09-27 20:55:27 -0400

    Partial Scatterplot
    Scatterplot started, but stacking of variables is incorrect

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -5,8 +5,7 @@ library('tidyverse')
 # Import CSV as tibble
 a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_for_Comparison_Project.csv", col_names = FALSE)
 
-# rename(a, X1=)
-
 # Scatterplot
 ggplot(data = a) +
-  geom_point(mapping = aes(x = X1, y = X2), color = 'blue')
+  geom_point(mapping = aes(x = X1, y = X2), color = 'blue') +
+  geom_point(mapping = aes(x = X1, y = X3, color = 'red'))
