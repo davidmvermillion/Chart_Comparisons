064247bb1784bf6594b3c88325207ab55006323b
David M Vermillion <social@davidmvermillion.com>
2020-09-28 07:32:42 -0400

    Checkin
    Should be relatively nothing, but still saving

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -4,8 +4,13 @@ library('tidyverse')
 
 # Import CSV as tibble
 a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_for_Comparison_Project.csv", col_names = FALSE)
+b <- as.data.frame(a)
+b <- t(b)
+b <- as.tibble(b)
 
 # Scatterplot
 ggplot(data = a) +
   geom_point(mapping = aes(x = X1, y = X2), color = 'blue') +
   geom_point(mapping = aes(x = X1, y = X3, color = 'red'))
+
+ggplot2::mpg
