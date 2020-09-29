386861bace89a996a8fb6c2d8d25c10b69dfdd31
David M Vermillion <social@davidmvermillion.com>
2020-09-29 08:02:45 -0400

    Next Day save

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -2,15 +2,18 @@
 
 library('tidyverse')
 
+names <- c("Datapoint", "Series1","Series2")
+
 # Import CSV as tibble
-a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_for_Comparison_Project.csv", col_names = FALSE)
-b <- as.data.frame(a)
-b <- t(b)
-b <- as.tibble(b)
+a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_for_Comparison_Project.csv", col_names = names)
+a <- as.numeric(a)
+b <- pivot_wider(a)
+
+
 
 # Scatterplot
 ggplot(data = a) +
-  geom_point(mapping = aes(x = X1, y = X2), color = 'blue') +
-  geom_point(mapping = aes(x = X1, y = X3, color = 'red'))
+  geom_point(mapping = aes(x = Datapoint, y = Series1), color = 'blue') #+
+  #geom_point(mapping = aes(x = Datapoint, y = Series2, color = 'red'))
 
 ggplot2::mpg
