6b8477119fc01574019dd02c9a41b77b390a59fe
David M Vermillion <social@davidmvermillion.com>
2020-09-29 21:30:19 -0400

    Basic Scatterplot
    Scaffolding created

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -6,14 +6,10 @@ names <- c("Datapoint", "Series1","Series2")
 
 # Import CSV as tibble
 a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_for_Comparison_Project.csv", col_names = names)
-a <- as.numeric(a)
-b <- pivot_wider(a)
-
-
 
 # Scatterplot
-ggplot(data = a) +
-  geom_point(mapping = aes(x = Datapoint, y = Series1), color = 'blue') #+
-  #geom_point(mapping = aes(x = Datapoint, y = Series2, color = 'red'))
-
-ggplot2::mpg
+# Hint to finally get the variables treated correctly from here
+# https://www.sixhat.net/how-to-plot-multpile-data-series-with-ggplot.html
+ggplot(a, aes(Datapoint, y = value, color = variable)) +
+  geom_point(aes(y = Series1, col = 'Series1')) +
+  geom_point(aes(y = Series2, col = 'Series2'))
