97b17052d9c44886683b2461c170762e7fcd6374
David M Vermillion <social@davidmvermillion.com>
2020-09-29 22:48:43 -0400

    Final change for 9/29

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -12,10 +12,16 @@ a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_f
 # https://www.sixhat.net/how-to-plot-multpile-data-series-with-ggplot.html
 # Hint for fitline
 # http://www.sthda.com/english/wiki/ggplot2-scatter-plots-quick-start-guide-r-software-and-data-visualization
-ggplot(a, aes(Datapoint, y = value, color = variable)) +
+ggplot(a, aes(x = Datapoint, y = value, color = variable)) +
   geom_point(aes(y = Series1, col = 'Series1')) +
   geom_point(aes(y = Series2, col = 'Series2')) +
   xlab('Datapoint') +
   ylab('Datapoint Value')
 
-#
+# Pulling from here for debugging linear fit
+# http://www.cookbook-r.com/Graphs/Scatterplots_(ggplot2)/
+
+ggplot(a, aes(x=Datapoint, y=Series1)) +
+  geom_point(shape=1) +    # Use hollow circles
+  geom_smooth(method=lm,   # Add linear regression line
+              se=TRUE)    # Don't add shaded confidence region
