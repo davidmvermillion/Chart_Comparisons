ed256944b63c359740ab43a9ccdf2cfc5a9a6fcb
David M Vermillion <social@davidmvermillion.com>
2020-09-29 22:40:56 -0400

    Figuring out how to get the linear fit to work

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -10,6 +10,12 @@ a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_f
 # Scatterplot
 # Hint to finally get the variables treated correctly from here
 # https://www.sixhat.net/how-to-plot-multpile-data-series-with-ggplot.html
+# Hint for fitline
+# http://www.sthda.com/english/wiki/ggplot2-scatter-plots-quick-start-guide-r-software-and-data-visualization
 ggplot(a, aes(Datapoint, y = value, color = variable)) +
   geom_point(aes(y = Series1, col = 'Series1')) +
-  geom_point(aes(y = Series2, col = 'Series2'))
+  geom_point(aes(y = Series2, col = 'Series2')) +
+  xlab('Datapoint') +
+  ylab('Datapoint Value')
+
+#
