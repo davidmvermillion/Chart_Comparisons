39b8ba0e40f12ebb0aa0cf66ab03e79392029212
David M Vermillion <social@davidmvermillion.com>
2020-10-05 14:51:35 -0400

    First working ggplot2 scatterplot
    Still missing equation for linear fit

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -12,33 +12,16 @@ a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_f
 # https://www.sixhat.net/how-to-plot-multpile-data-series-with-ggplot.html
 # Hint for fitline
 # http://www.sthda.com/english/wiki/ggplot2-scatter-plots-quick-start-guide-r-software-and-data-visualization
-ggplot(a, aes(x = Datapoint, y = value, color = variable)) +  # Setup
-  geom_point(aes(y = Series1, col = 'Series1')) +             # Series 1 plot
-  geom_point(aes(y = Series2, col = 'Series2')) +             # Series 2 plot
-  labs(title = 'example', xlab = 'Datapoint', ylab = 'Datapoint Value')   # Title and axes labels
-
-# Trying hints from here
+# Tried hints from here
 # https://stackoverflow.com/questions/31410230/multiple-linear-regression-for-a-dataset-in-r-with-ggplot2
-ggplot(a) +
-  aes(x = Datapoint, y = value, color = variable) +
-  geom_point()
-  
-  
-
-ggplot(a,aes(x = Datapoint, y = Series1, col='blue')) +
-  geom_point() + geom_smooth(method=lm)
-
-ggplot(a, aes(x=Datapoint, y = Series2, col = 'red')) +
-  geom_point() + geom_smooth(method=lm)
-
-ggplot(a, aes(x = Datapoint, y = value)) +
-  geom_point(aes(y = Series1, color = 'blue'))
-
-
-# Pulling from here for debugging linear fit
+# and from here
 # http://www.cookbook-r.com/Graphs/Scatterplots_(ggplot2)/
+# Finally figured it out with hint from Duck
+# https://stackoverflow.com/questions/64213270/how-do-i-make-a-linear-fit-line-in-r-with-ggplot2-for-multiple-y-series/64213351#64213351
 
-ggplot(a, aes(x=Datapoint, y=Series1)) +
-  geom_point(shape=1) +    # Use hollow circles
-  geom_smooth(method=lm,   # Add linear regression line
-              se=TRUE)    # Don't add shaded confidence region
+a %>% pivot_longer(-Datapoint) %>%
+  ggplot(aes(x = Datapoint, y = value, color = name, group = name)) +                   # Setup the background
+  geom_point() +                                                                        # Scatterplot
+  geom_smooth(method = lm, se = FALSE) +                                                # Linear fit
+  labs(title = 'Randomized Data Points', xlab = 'Datapoint', ylab = 'Datapoint Value')  # Labels
+
