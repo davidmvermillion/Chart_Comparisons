fdcbc670d20a5db3f108d01911390ddf35a86891
David M Vermillion <social@davidmvermillion.com>
2020-10-05 15:15:23 -0400

    ggplot2 Scatterplot Complete
    Input from Duck and found a library to help simplify labeling the regression line.

--- ggplot2Demo.R
+++ ggplot2Demo.R
@@ -1,6 +1,7 @@
 # Load the tidyverse, which includes ggplot2
 
 library('tidyverse')
+library('ggpmisc')
 
 names <- c("Datapoint", "Series1","Series2")
 
@@ -18,10 +19,12 @@ a <- read_csv("C:/Users/david/Documents/GitHub/Chart_Comparisons/Seeded_Values_f
 # http://www.cookbook-r.com/Graphs/Scatterplots_(ggplot2)/
 # Finally figured it out with hint from Duck
 # https://stackoverflow.com/questions/64213270/how-do-i-make-a-linear-fit-line-in-r-with-ggplot2-for-multiple-y-series/64213351#64213351
-
+# Found package ggpmisc for linear fit equation from this conversation
+# https://stackoverflow.com/questions/7549694/add-regression-line-equation-and-r2-on-graph
+my.formula <- y ~ x # Required for linear equation listing
 a %>% pivot_longer(-Datapoint) %>%
-  ggplot(aes(x = Datapoint, y = value, color = name, group = name)) +                   # Setup the background
-  geom_point() +                                                                        # Scatterplot
-  geom_smooth(method = lm, se = FALSE) +                                                # Linear fit
-  labs(title = 'Randomized Data Points', xlab = 'Datapoint', ylab = 'Datapoint Value')  # Labels
-
+  ggplot(aes(x = Datapoint, y = value, color = name, group = name)) +                                             # Setup the background
+  geom_point() +                                                                                                  # Scatterplot
+  geom_smooth(method = lm, se = FALSE) +                                                                          # Linear fit
+  labs(title = 'Randomized Data Points', xlab = 'Datapoint', ylab = 'Datapoint Value') +                          # Labels
+  stat_poly_eq(formula = my.formula, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), parse = TRUE)   # Equation for linear fit
